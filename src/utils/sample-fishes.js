1906c12fd4f9f55b55ba1d1f0b70480979b5da49
Vaibhav Sompura <42895093+vsompura3@users.noreply.github.com>
2023-03-11 22:30:17 +0530

    added sample data image urls

--- src/utils/sample-fishes.js
+++ src/utils/sample-fishes.js
@@ -2,7 +2,8 @@
 const fishes = {
   fish1: {
     name: 'Pacific Halibut',
-    image: '/images/hali.jpg',
+    image:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQVU-I7wMJ-CLLsexOyJcPAsRMBKjm7DJhRWN_6gHRzHg&usqp=CAU&ec=48600112',
     desc: 'Everyone’s favorite white fish. We will cut it to the size you need and ship it.',
     price: 1724,
     status: 'available',
@@ -10,7 +11,8 @@ const fishes = {
 
   fish2: {
     name: 'Lobster',
-    image: '/images/lobster.jpg',
+    image:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQfhHh0HqJHvrEj1CJ5i3OO9Zs_9Ru91syOeAzcjRFijQ&usqp=CAU&ec=48600112',
     desc: 'These tender, mouth-watering beauties are a fantastic hit at any dinner party.',
     price: 3200,
     status: 'available',
@@ -18,7 +20,8 @@ const fishes = {
 
   fish3: {
     name: 'Sea Scallops',
-    image: '/images/scallops.jpg',
+    image:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT3tm1D7KZJE1nWdE8I11g1MIWY-b2m6mgJCr1SNXQpzw&usqp=CAU&ec=48600112',
     desc: 'Big, sweet and tender. True dry-pack scallops from the icey waters of Alaska. About 8-10 per pound',
     price: 1684,
     status: 'unavailable',
@@ -26,7 +29,8 @@ const fishes = {
 
   fish4: {
     name: 'Mahi Mahi',
-    image: '/images/mahi.jpg',
+    image:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQc3AOJVzXuqmMzTiFmdW4HBZPaUiKHUTNWmnKf_HHPFw&usqp=CAU&ec=48600112',
     desc: 'Lean flesh with a mild, sweet flavor profile, moderately firm texture and large, moist flakes. ',
     price: 1129,
     status: 'available',
@@ -34,7 +38,8 @@ const fishes = {
 
   fish5: {
     name: 'King Crab',
-    image: '/images/crab.jpg',
+    image:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS_FWfi7LNfzx_uCcUlCemZ9WPgEQXRynVVERVrH3WnBA&usqp=CAU&ec=48600112',
     desc: 'Crack these open and enjoy them plain or with one of our cocktail sauces',
     price: 4234,
     status: 'available',
@@ -42,7 +47,8 @@ const fishes = {
 
   fish6: {
     name: 'Atlantic Salmon',
-    image: '/images/salmon.jpg',
+    image:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRjLBrdg-ubqLPcBeXrOnqjuALqDmo8HqEoVTZfEEePIQ&usqp=CAU&ec=48600112',
     desc: 'This flaky, oily salmon is truly the king of the sea. Bake it, grill it, broil it...as good as it gets!',
     price: 1453,
     status: 'available',
@@ -50,7 +56,8 @@ const fishes = {
 
   fish7: {
     name: 'Oysters',
-    image: '/images/oysters.jpg',
+    image:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSY8A1J3l1sGa83BcCndCXM-h8G18wOU68C4h1wJhongQ&usqp=CAU&ec=48600112',
     desc: 'A soft plump oyster with a sweet salty flavor and a clean finish.',
     price: 2543,
     status: 'available',
@@ -58,7 +65,8 @@ const fishes = {
 
   fish8: {
     name: 'Mussels',
-    image: '/images/mussels.jpg',
+    image:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQc3AOJVzXuqmMzTiFmdW4HBZPaUiKHUTNWmnKf_HHPFw&usqp=CAU&ec=48600112',
     desc: 'The best mussels from the Pacific Northwest with a full-flavored and complex taste.',
     price: 425,
     status: 'available',
@@ -66,7 +74,8 @@ const fishes = {
 
   fish9: {
     name: 'Jumbo Prawns',
-    image: '/images/prawns.jpg',
+    image:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRx97rSjpb8toBRaeO6uzPos_yNVEhKgxVWbDUOKsA1xA&usqp=CAU&ec=48600112',
     desc: 'With 21-25 two bite prawns in each pound, these sweet morsels are perfect for shish-kabobs.',
     price: 2250,
     status: 'available',
