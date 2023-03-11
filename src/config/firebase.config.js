69d82b202f4d739dc6fa025ff12f8c79503f4bd9
Vaibhav Sompura <42895093+vsompura3@users.noreply.github.com>
2023-03-11 22:11:57 +0530

    fixed env issues

--- src/config/firebase.config.js
+++ src/config/firebase.config.js
@@ -17,8 +17,6 @@ const firebaseConfig = {
   measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 }
 
-console.log(import.meta.env)
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig)
 const db = getDatabase(app)
